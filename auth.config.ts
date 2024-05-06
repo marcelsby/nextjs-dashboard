f32e925f31670a6584098a3413e5cd52fd8f7b02
Marcel Barbosa <marcel.sby.br@gmail.com>
2024-05-06 09:56:56 -0300

    Chapter 17

--- auth.config.ts
+++ auth.config.ts
@@ -14,8 +14,6 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-
-        console.log(isLoggedIn);
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
